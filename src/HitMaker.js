d86352ee32ff7c5838e347287bb916a4e3c88a7a
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2016-06-30 16:44:22 +0100

    it makes a horrid noise

--- src/HitMaker.js
+++ src/HitMaker.js
@@ -1,20 +1,20 @@
-var hits = []
-
-function generateHits () {
-    var totalHits = 1000
-    for(var i = 0; i < totalHits; i++) {
-        hits.push(generateRandomHit)
-    }
+export function generateHits () {
+  var hits = []
+  var totalHits = 1000
+  for (var i = 0; i < totalHits; i++) {
+    hits.push(generateRandomHit())
+  }
+  return hits
 }
 
-function generateRandomHit() {
-    return {
-        t: 4 * Math.random(),
-        f: Math.random(),
-        v: Math.random()
-    }
+function generateRandomHit () {
+  return {
+    t: 4 * Math.random(),
+    f: Math.random(),
+    v: Math.random()
+  }
 }
 
-function scheduleHit(h) {
-    
+function scheduleHit (h) {
+
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
+import { generateHits } from './HitMaker'
+
+var actx
 
 class Example extends Component {
+  componentDidMount() {
+    actx = new AudioContext()
+    var hits = generateHits()
+    for (var i = 0; i < hits.length; i ++) {
+      this.scheduleHit(hits[i])
+    }
+  }
+  scheduleHit(h) {
+    var l = 0.2
+    var oscillator = actx.createOscillator();
+    oscillator.frequency.value = 50 + 1000 * h.f
+    var gain = actx.createGain()
+    oscillator.connect(gain)
+    gain.connect(actx.destination)
+    var now = actx.currentTime
+    gain.gain.setValueAtTime(0.02 * h.v, now + h.t)
+    gain.gain.exponentialRampToValueAtTime(0.001, now + h.t + l)
+    oscillator.start(now + h.t)
+    oscillator.stop(now + h.t + l)
+  }
   render () {
     return <h1>This is not yet a drum machine</h1>
   }
