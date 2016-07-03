fbfea9a592274210009a9ed8ae7bf4529a87ae16
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2016-07-03 14:42:03 +0100

    making noise

--- better/script.js
+++ better/script.js
@@ -1,2 +1,34 @@
 // bored of this reactpack bullshit
 // just playing around here for now
+
+var settings = {
+  numChannels: 2,
+  bottomFreq: 10,
+  topFreq: 450,
+  releaseTime: 0.5
+}
+
+var a = new AudioContext()
+
+function createNoiseNode() {
+  var bufferSize = 1000000 // is this too big?
+  var node = a.createBufferSource()
+  var buffer = a.createBuffer(1, bufferSize, a.sampleRate)
+  var data = buffer.getChannelData(0)
+
+  for (var i = 0; i < bufferSize; i++) {
+    data[i] = 0.05 * Math.random()
+  }
+
+  node.buffer = buffer
+  node.loop = true
+  node.start()
+
+  return node
+}
+
+var nodes = []
+for (var i = 0; i < settings.numChannels; i ++) {
+  nodes[i] = createNoiseNode()
+  //nodes[i].connect(a.destination)
+}
