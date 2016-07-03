a0848490889e8499efed0e9cfeb2fec80c1060fc
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2016-07-03 16:51:28 +0100

    changes afoot

--- better/script.js
+++ better/script.js
@@ -3,16 +3,22 @@
 
 var settings = {
   numChannels: 5,
-  bottomFreq: 2000,
-  topFreq: 100,
+  bottomFreq: 500,
+  topFreq: 600,
   releaseTime: 0.5
 }
 
 var a = new AudioContext()
+var bufferSize = 1000 // tweak this?
+var noiseBuffer = a.createBuffer(1, bufferSize, a.sampleRate)
+var noiseData = noiseBuffer.getChannelData(0)
+for (var i = 0; i < bufferSize; i++) {
+  noiseData[i] = Math.random()
+}
 
 function createNoiseNode(freq) {
 
-  var bufferSize = 1000000 // is this too big?
+  var bufferSize = 10000 // is this too big?
   var node = a.createBufferSource()
   var buffer = a.createBuffer(1, bufferSize, a.sampleRate)
   var data = buffer.getChannelData(0)
@@ -31,7 +37,11 @@ function createNoiseNode(freq) {
   node.connect(filter)
 
   var gain = a.createGain()
-  gain.gain.value = Math.min(1, 1/(freq/100)) / settings.numChannels
+  gain.gain.value = 0
+  //gain.gain.value = Math.min(1, 1/(freq/100)) / settings.numChannels
+
+  //gain.gain.setValueAtTime(gain.gain.value, a.currentTime + 2)
+  //gain.gain.exponentialRampToValueAtTime(0.00001, a.currentTime + 5)
 
   filter.connect(gain)
 
@@ -43,3 +53,27 @@ for (var i = 0; i < settings.numChannels; i ++) {
   nodes[i] = createNoiseNode(settings.bottomFreq + i * (settings.topFreq - settings.bottomFreq) / (settings.numChannels - 1))
   nodes[i].connect(a.destination)
 }
+
+var scheduledTo = 0
+var schedulePeriod = 10
+var timeoutPeriod = 1
+var notesPerSecond = 10
+function scheduleNotes() {
+  var thisSchedulePeriod = schedulePeriod - (scheduledTo - a.currentTime)
+  console.log(thisSchedulePeriod)
+  var totalNotes = notesPerSecond * thisSchedulePeriod
+  for(var i = 0; i < totalNotes; i++) {
+    scheduleNote(scheduledTo + Math.random() * thisSchedulePeriod)
+  }
+  scheduledTo = a.currentTime + schedulePeriod
+}
+
+function scheduleNote(t) {
+  console.log(t)
+  var node = a.createBufferSource()
+  node.buffer = noiseBuffer
+  node.connect(a.destination)
+  node.start(t)
+}
+
+scheduleNotes()
