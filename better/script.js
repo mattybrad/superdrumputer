112895d31f34125242fecac73e8bd26fb8ca260b
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2016-07-03 15:37:56 +0100

    rough equal power volume stuff

--- better/script.js
+++ better/script.js
@@ -2,33 +2,44 @@
 // just playing around here for now
 
 var settings = {
-  numChannels: 2,
-  bottomFreq: 10,
-  topFreq: 450,
+  numChannels: 5,
+  bottomFreq: 2000,
+  topFreq: 100,
   releaseTime: 0.5
 }
 
 var a = new AudioContext()
 
-function createNoiseNode() {
+function createNoiseNode(freq) {
+
   var bufferSize = 1000000 // is this too big?
   var node = a.createBufferSource()
   var buffer = a.createBuffer(1, bufferSize, a.sampleRate)
   var data = buffer.getChannelData(0)
 
   for (var i = 0; i < bufferSize; i++) {
-    data[i] = 0.05 * Math.random()
+    data[i] = Math.random()
   }
 
   node.buffer = buffer
   node.loop = true
   node.start()
 
-  return node
+  var filter = a.createBiquadFilter()
+  filter.type = "bandpass"
+  filter.frequency.value = freq
+  node.connect(filter)
+
+  var gain = a.createGain()
+  gain.gain.value = Math.min(1, 1/(freq/100)) / settings.numChannels
+
+  filter.connect(gain)
+
+  return gain
 }
 
 var nodes = []
 for (var i = 0; i < settings.numChannels; i ++) {
-  nodes[i] = createNoiseNode()
-  //nodes[i].connect(a.destination)
+  nodes[i] = createNoiseNode(settings.bottomFreq + i * (settings.topFreq - settings.bottomFreq) / (settings.numChannels - 1))
+  nodes[i].connect(a.destination)
 }
