38369fa73bceec9cf8ba8e443df47296137d014d
Matt Bradshaw <matthewloydbradshaw@gmail.com>
2016-07-03 17:12:26 +0100

    sounds like fireworks

--- better/script.js
+++ better/script.js
@@ -3,13 +3,13 @@
 
 var settings = {
   numChannels: 5,
-  bottomFreq: 500,
-  topFreq: 600,
+  bottomFreq: 100,
+  topFreq: 800,
   releaseTime: 0.5
 }
 
 var a = new AudioContext()
-var bufferSize = 1000 // tweak this?
+var bufferSize = 100000 // tweak this?
 var noiseBuffer = a.createBuffer(1, bufferSize, a.sampleRate)
 var noiseData = noiseBuffer.getChannelData(0)
 for (var i = 0; i < bufferSize; i++) {
@@ -48,31 +48,56 @@ function createNoiseNode(freq) {
   return gain
 }
 
-var nodes = []
+var channels = []
 for (var i = 0; i < settings.numChannels; i ++) {
-  nodes[i] = createNoiseNode(settings.bottomFreq + i * (settings.topFreq - settings.bottomFreq) / (settings.numChannels - 1))
-  nodes[i].connect(a.destination)
+  channels[i] = createChannel(settings.bottomFreq + i * (settings.topFreq - settings.bottomFreq) / (settings.numChannels - 1))
+  channels[i].output.connect(a.destination)
+}
+
+function createChannel(freq) {
+  var filter = a.createBiquadFilter()
+  filter.type = "bandpass"
+  filter.frequency.value = freq
+
+  var gain = a.createGain()
+  gain.gain.value = 0
+  gain.gain.value = Math.min(1, 1/(freq/100)) / settings.numChannels
+
+  filter.connect(gain)
+
+  return {
+    input: filter,
+    output: gain
+  }
 }
 
 var scheduledTo = 0
-var schedulePeriod = 10
-var timeoutPeriod = 1
-var notesPerSecond = 10
+var schedulePeriod = 1
+var timeoutPeriod = 0.5
+var notesPerSecond = 20
 function scheduleNotes() {
   var thisSchedulePeriod = schedulePeriod - (scheduledTo - a.currentTime)
-  console.log(thisSchedulePeriod)
   var totalNotes = notesPerSecond * thisSchedulePeriod
   for(var i = 0; i < totalNotes; i++) {
-    scheduleNote(scheduledTo + Math.random() * thisSchedulePeriod)
+    scheduleNote(scheduledTo + Math.random() * thisSchedulePeriod, Math.floor(Math.random() * settings.numChannels))
   }
   scheduledTo = a.currentTime + schedulePeriod
+  setTimeout(scheduleNotes, timeoutPeriod * 1000)
 }
 
-function scheduleNote(t) {
+function scheduleNote(t, channel) {
   console.log(t)
   var node = a.createBufferSource()
   node.buffer = noiseBuffer
-  node.connect(a.destination)
+
+  var gain = a.createGain()
+  gain.gain.value = 0
+  gain.gain.setValueAtTime(Math.random(), t)
+  gain.gain.exponentialRampToValueAtTime(0.00001, t + 5)
+
+  node.connect(gain)
+  gain.connect(channels[channel].input)
+
   node.start(t)
 }
 
